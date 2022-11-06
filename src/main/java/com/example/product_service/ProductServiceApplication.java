a0b884645505310e2d92811d29442044d9d9f093
philipdaquin <philipdaquin20@gmail.com>
2022-11-06 08:00:11 +0800

    swagger ui

--- src/main/java/com/example/product_service/ProductServiceApplication.java
+++ src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.example.product_service.config.SwaggerConfiguration;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
